de67e8b3733bdc6e3ed0516097df0b18adac0ad5
Florian Matter <florianmatter@gmail.com>
2022-05-27 11:39:44 -0700

    release 0.0.2

--- src/clld_corpus_plugin/__init__.py
+++ src/clld_corpus_plugin/__init__.py
@@ -7,7 +7,7 @@ from pyramid.response import Response, FileResponse
 
 __author__ = "Florian Matter"
 __email__ = "florianmatter@gmail.com"
-__version__ = "0.0.2.dev"
+__version__ = "0.0.2"
 
 audio_suffixes = [".mp3", ".wav"]
 
