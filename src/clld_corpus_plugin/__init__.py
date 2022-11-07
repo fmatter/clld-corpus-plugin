2e3cfe62ff0f5f0f4cfb5ef22a4a8b43e793dbbf
Florian Matter <florianmatter@gmail.com>
2022-11-06 19:07:30 -0800

    bump

--- src/clld_corpus_plugin/__init__.py
+++ src/clld_corpus_plugin/__init__.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 
 __author__ = "Florian Matter"
 __email__ = "florianmatter@gmail.com"
-__version__ = "0.0.6"
+__version__ = "0.0.7.dev"
 
 audio_suffixes = [".mp3", ".wav"]
 
